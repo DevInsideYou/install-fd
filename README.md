8b0e05d1cef7e38f65d3cac88c43b06388209ded
Vladyslav Pekker <agilesteel@gmail.com>
2020-07-16 15:09:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installs [fd 8.1.1](https://github.com/sharkdp/fd) on [Ubuntu](https://www.ubuntu.com/)
+
+```bash
+wget -q https://script.install.devinsideyou.com/fd
+sudo chmod +x fd && ./fd
+```
+
+# Specifies [version](https://github.com/sharkdp/fd/releases) (optional)
+
+```bash
+wget -q https://script.install.devinsideyou.com/fd
+sudo chmod +x fd && ./fd 8.1.1
+```
---
+++ install-fd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# remove yourself
+rm $0
+
+FALLBACK_VERSION="8.1.1"
+
+if [ "$1" != "" ]; then
+    VERSION="$1"
+else
+    VERSION=$FALLBACK_VERSION
+fi
+
+DEBIAN_PACKAGE=fd_${VERSION}_amd64.deb
+
+# install fd
+wget https://github.com/sharkdp/fd/releases/download/v${VERSION}/$DEBIAN_PACKAGE
+sudo dpkg -i $DEBIAN_PACKAGE
+rm $DEBIAN_PACKAGE
+
+echo
+echo '"fd" is now on the path'
